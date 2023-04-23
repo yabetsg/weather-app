f3030e8576b04551b8dde5cd91c70dbb9ed0d529
yabets <yabets.g2@gmail.com>
2023-04-23 15:08:37 -0400

    Implement basic weather features: current,humidity,feels like

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,49 @@
-import { getWeatherData , getUserInput} from "./api";
+import { getWeatherData } from "./api";
+import { getUserInput } from ".";
 
-export const displayCurrent = () => {
+ export const displayCurrent = async (data) => {
     const currentWeather = document.getElementById('current-weather');
-    const response = getUserInput()
-    console.log(getUserInput());
+    currentWeather.textContent = `${data}°F`;
+   
 }
+
+ export const displayRegion = async (data)=>{
+    const region = document.getElementById('city-name');
+    region.textContent = data;
+    
+}
+ export const displayFeelsLike = async (data) =>{
+    const feelsLike = document.getElementById('feels-like');
+    feelsLike.textContent = `${data}°F`;
+ }
+ export const displayHumidity = async (data) =>{
+    const humidity = document.getElementById('humidity');
+    humidity.textContent = `${data}%`;
+   
+}
+
+export const displayWeatherData = async ()=>{
+    const currentWeather = document.getElementById('current-weather');
+    const region = document.getElementById('city-name');
+    const feelsLike = document.getElementById('feels-like');
+    const humidity = document.getElementById('humidity');
+    const response = await getUserInput();
+    currentWeather.textContent = `${response.current.temp_f}°F`;
+    region.textContent = response.location.name;
+    feelsLike.textContent = `${response.current.feelslike_f}°F`;
+    humidity.textContent = `${response.current.humidity}%`;
+};
+
+window.onload = async (e)=>{
+    const currentWeather = document.getElementById('current-weather');
+    const region = document.getElementById('city-name');
+    const feelsLike = document.getElementById('feels-like');
+    const humidity = document.getElementById('humidity');
+    const url = "https://api.weatherapi.com/v1/current.json?key=5970c03ba0ec486bad4223716232104&q=detroit"
+    const response = await getWeatherData(url);
+    currentWeather.textContent = `${response.current.temp_f}°F`;
+    region.textContent = response.location.name;
+    feelsLike.textContent = `Feels like: ${response.current.feelslike_f}°F`;
+    humidity.textContent = `Humidity: ${response.current.humidity}%`;
+}
+// displayWeatherData();
--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-import { getWeatherData } from "./api"; 
-import { displayCurrent } from "./dom";
+import { getWeatherData,buildURL } from "./api"; 
+import { displayCurrent,displayFeelsLike,displayHumidity,displayRegion } from "./dom";
+
+export const getUserInput = async () => {
+    const form = document.querySelector("form");
+  const promise = new Promise((resolve) => {
+    form.addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const userInput = document.getElementById("city");
+      let city = userInput.value;
+      const url = buildURL(city); 
+       const response = await getWeatherData(url);
+       displayRegion(response.location.name)
+       displayCurrent(response.current.temp_f);
+       displayFeelsLike(response.current.feelslike_f);
+       displayHumidity(response.current.humidity);
+       resolve(response);
+       userInput.value = '';
+    });
+  });
+   
+};
+getUserInput();
